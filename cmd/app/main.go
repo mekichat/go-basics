4dda9a39e598f08e1878b021a44da7246477883a
Yahya9102 <yaya_hussein@hotmail.com>
2026-02-10 16:46:03 +0100

    project setup

---
+++ cmd/app/main.go
@@ -0,0 +1,13 @@
+package app
+
+/*
+	Skapa en user-registry i CLI
+
+	Create = Skapa ny användare
+	List = Lista alla användare
+
+	Övning
+	Delete -> Radera en användare
+	Update -> Uppdatera en användare
+
+*/
